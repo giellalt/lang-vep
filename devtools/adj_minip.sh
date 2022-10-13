d439d55435a81faa4c54285a5b457eff9f6578ca
Trond Trosterud <trond.trosterud@uit.no>
2022-10-13 16:07:37 +0200

    testdocuments+prcedures

---
+++ devtools/adj_minip.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# script to generate paradigms for generating word forms
+# command:
+# sh generate_contlex_para.sh PATTERN
+# example, when you are in vep:
+# sh devtools/adj_minip.sh a_22odd  | less
+# sh devtools/adj_minip.sh lestadiolainen 
+# Only get the lemma you ask for:
+# sh devtools/adj_minip.sh '^lestadiolainen[ :+]' 
+
+LOOKUP=$(echo $LOOKUP)
+GTLANGS=$(echo $GTLANGS)
+
+PATTERN=$1
+L_FILE="in.txt"
+cut -d '!' -f1 src/fst/stems/adjectives.lexc | egrep $PATTERN | sed 's/% /%/g' | tr ' +' ':' | cut -d ':' -f1 | sed 's/%/% /g' | tr -d '%'>$L_FILE
+
+P_FILE="test/data/testadjparadigm.txt"
+
+for lemma in $(cat $L_FILE);
+do
+ for form in $(cat $P_FILE);
+ do
+   echo "${lemma}${form}" | $LOOKUP $GTLANGS/lang-vep/src/generator-gt-norm.xfst
+  # echo "${lemma}${form}" | $LOOKUP $GTLANGS/lang-vep/src/generator-gt-norm.xfst
+ done
+ rm -f $L_FILE
+done
+
+
---
+++ devtools/verb_minip.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# script to generate paradigms for generating word forms
+# command, when you are in vep:
+# sh devtools/verb_minip.sh v43 | less
+# sh devtools/verb_minip.sh silitellä 
+
+
+LOOKUP=$(echo $LOOKUP)
+HLOOKUP=$(echo $HLOOKUP)
+GTLANGS=$(echo $GTLANGS)
+
+
+PATTERN=$1
+L_FILE="in.txt"
+cut -d '!' -f1 src/fst/stems/verbs.lexc | egrep $PATTERN | sed 's/% /%/g' | tr ' +' ':' | cut -d ':' -f1 | sed 's/%/% /g' | tr -d '%'>$L_FILE
+
+
+P_FILE="test/data/testverbparadigm.txt"
+
+for lemma in $(cat $L_FILE);
+do
+ for form in $(cat $P_FILE);
+ do
+    #  echo "${lemma}${form}" | $LOOKUP $GTLANGS/lang-vep/src/generator-gt-norm.xfst
+   echo "${lemma}${form}" | $HLOOKUP $GTLANGS/lang-vep/src/generator-gt-norm.hfstol
+ done
+ rm -f $L_FILE
+done
+
