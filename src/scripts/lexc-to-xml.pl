631e0d9013aae9cada3efac5f7fb4c88ee9bd8f4
Trond Trosterud <trond.trosterud@uit.no>
2024-06-07 11:50:57 +0200

    script for moving the popular newword files into xml format (note: the script must include translations should they show up

---
+++ src/scripts/lexc-to-xml.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/perl -w
+use utf8 ;
+
+# Simple script to convert csv to xml
+# For input/outpus examples, see below.
+
+
+print STDOUT "<r>\n";
+
+while (<>) 
+{
+	chomp ;
+	my ($lemma, $POS, $stem, $contlex) = split /\t/ ;
+	print STDOUT "   <e>\n";
+	print STDOUT "      <lg>\n";
+	print STDOUT "         <l pos=\"$POS\">$lemma</l>\n";
+	print STDOUT "         <stg>\n";
+	print STDOUT "            <st Contlex=\"$contlex\">$stem</st>\n";
+	print STDOUT "         </stg>\n";
+	print STDOUT "      </lg>\n";
+	print STDOUT "      <mg>\n";
+	print STDOUT "         <tg xml:lang=\"fin\">\n";
+	print STDOUT "            <t pos=\"$POS\"></t>\n";
+	print STDOUT "         </tg>\n";
+	print STDOUT "      </mg>\n";
+	print STDOUT "   </e>\n";
+}
+
+print STDOUT "</r>\n";
+
+
+
+# Example input:
+#
+# aampumakenttä	N	skytefelt
+
+
+#Target output:
+#
+#   <e src="yr">
+#      <lg>
+#         <l pos="N">aampumakenttä</l>
+#      </lg>
+#      <mg>
+#         <tg>
+#            <t pos="N" gen="x">skytefelt</t>
+#         </tg>
+#      </mg>
+#   </e>
+#
+
