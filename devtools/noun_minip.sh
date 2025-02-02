b9eb71e1565f24428701ba883915b43ed480e417
Trond Trosterud <trond.trosterud@uit.no>
2025-02-02 21:02:37 +0100

    hfst

--- devtools/noun_minip.sh
+++ devtools/noun_minip.sh
@@ -17,7 +17,7 @@ GTLANGS=$(echo $GTLANGS)
 
 PATTERN=$1
 L_FILE="in.txt"
-cut -d '!' -f1 src/fst/stems/nouns_newwords.lexc | egrep $PATTERN | sed 's/% /%/g' | tr ' +' ':' | cut -d ':' -f1 | sed 's/%/% /g' | tr -d '%'>$L_FILE
+cut -d '!' -f1 src/fst/morphology/stems/nouns_newwords.lexc | egrep $PATTERN | sed 's/% /%/g' | tr ' +' ':' | cut -d ':' -f1 | sed 's/%/% /g' | tr -d '%'>$L_FILE
 
 
 P_FILE="test/data/testnounparadigm.txt"
@@ -26,8 +26,8 @@ for lemma in $(cat $L_FILE);
 do
  for form in $(cat $P_FILE);
  do
-#  echo "${lemma}${form}" | $LOOKUP $GTLANGS/lang-vep/src/generator-gt-norm.xfst
-  echo "${lemma}${form}" | $HLOOKUP $GTLANGS/lang-vep/src/generator-gt-norm.hfst
+#  echo "${lemma}${form}" | $LOOKUP $GTLANGS/lang-vep/src/fst/generator-gt-norm.xfst
+  echo "${lemma}${form}" | $HLOOKUP $GTLANGS/lang-vep/src/fst/generator-gt-norm.hfstol
  done
  rm -f $L_FILE
 done
